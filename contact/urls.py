16803f852191cdbec45af01c392ad76ef8342844
Stanislav Pisotskyi <spisotskyi@gmail.com>
2024-04-25 09:11:16 +0300

    contacts - invitations and confirm endpoints

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<int:target_id>/invite', views.invite)
+    path('/<int:target_id>/invite', views.invite),
+    path('/invitations', views.get_invitations),
+    path('/<int:initiator_id>/confirm', views.confirm)
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,11 +2,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes, permission_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.parsers import JSONParser
 from rest_framework import status
 from api_auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Contact, STATUS_WAITING
+from .models import Contact, STATUS_WAITING, STATUS_CONFIRMED
 from .serializers import ContactSerializer
 
 
@@ -38,3 +37,44 @@ def invite(request, target_id):
 
     serializer = ContactSerializer(contact)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+@permission_classes([IsAuthenticated])
+def get_invitations(request):
+    try:
+        invitations = Contact.objects.filter(target=request.user, status=STATUS_WAITING).order_by('-id')
+    except Contact.DoesNotExist:
+        invitations = None
+
+    serializer = ContactSerializer(invitations, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@renderer_classes([JSONRenderer])
+@permission_classes([IsAuthenticated])
+def confirm(request, initiator_id):
+    try:
+        initiator = User.objects.get(pk=initiator_id)
+    except ObjectDoesNotExist:
+        initiator = None
+
+    if not isinstance(initiator, User):
+        raise TypeError('Initiator is not found.')
+
+    try:
+        existed_contact = Contact.objects.get(initiator=initiator, target=request.user, status=STATUS_WAITING)
+    except ObjectDoesNotExist:
+        existed_contact = None
+
+    if not isinstance(existed_contact, Contact):
+        raise TypeError('Contact to confirm is not found.')
+
+    existed_contact.status = STATUS_CONFIRMED
+    existed_contact.save()
+
+    serializer = ContactSerializer(existed_contact)
+    return Response(serializer.data, status=status.HTTP_200_OK)
